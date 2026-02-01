d520f6665a19b4fa72d6e55e5dbaa07ed9ce5042
vtalla <t.vishal95@gmail.com>
2026-01-31 18:23:58 -0600

    check in 1

---
+++ list_dictionary_tupels.py
@@ -0,0 +1,13 @@
+# List
+cars = ["BMW", "Audi", "Tesla"]
+
+# Dictionary (like JSON / DB row)
+person = {
+    "name": "Vishal",
+    "age": 26,
+    "city": "Dallas"
+}
+
+print(person["name"], person["city"])
+
+
